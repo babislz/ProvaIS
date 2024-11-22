49628fe9396b90eb0b44e29527f6222aee45016a
babislz <gabriela.laureano2007@gmail.com>
2024-11-22 09:32:35 -0300

    aaaaaaaaa

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-const config = require('config')
+const {config} = require('dotenv')
+config()
 
 module.exports = function () {
-    const db = config.get('db');
+    const db = process.env.DB_URL;
     mongoose.connect(db)
         .then(() => console.log(`connected to ${db}`));
 }
